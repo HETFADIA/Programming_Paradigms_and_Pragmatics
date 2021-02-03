a9c4a9cc668c7b17a2a7922b94f2fef322688890
Het <62541263+HETFADIA@users.noreply.github.com>
2021-02-03 23:54:57 +0530

    Create Temperature.java

---
+++ Temperature.java
@@ -0,0 +1,9 @@
+public class Temperature {
+    public static void main(String[] args) {
+        double degFahrenheit;
+        double degCelsius;
+        degFahrenheit = 98.6;
+        degCelsius = (degFahrenheit - 32) / 1.8;
+        System.out.println(degFahrenheit + " degrees degFahrenheit equals " + degCelsius + " degrees degCelsius");
+    }
+}
