9e3ec64f8846c3eceacd9404179ea0589e550526
Het <62541263+HETFADIA@users.noreply.github.com>
2021-02-04 00:26:54 +0530

    Create InchesToFeet45.java

---
+++ InchesToFeet45.java
@@ -0,0 +1,8 @@
+public class InchesToFeet45 {
+    public static void main(String[] args) {
+        int no = 45;
+        int X = 45 / 12;
+        int Y = 45 % 12;
+        System.out.println("45 inches = " + X + " feet, " + Y + " inches");
+    }
+}
