84c99e950089ceda57a88c408403e0b99d7a44f2
Het <62541263+HETFADIA@users.noreply.github.com>
2021-02-14 17:40:42 +0530

    Add files via upload

---
+++ lab2_computer_good/TicTacToe.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String args[]) {
+
+        int playsAgain = 1;
+        while (playsAgain == 1) {
+            TwoPlayers.instructions();
+            int game = TwoPlayers.getGameType();
+
+            if (game == 1) {
+                TwoPlayers.TwoPlayerGame();
+            } else {
+                ComputerPlayerGame.ComputerGame();
+            }
+            playsAgain = TwoPlayers.continues();
+
+        }
+
+    }
+
+}
